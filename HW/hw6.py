784d0990684949e1096c5f70cfa21975d7588600
mrCSadil <samsievadil6@gmail.com>
2024-11-19 14:16:51 +0600

    Hw6 O(log n(n))

---
+++ HW/hw6.py
@@ -0,0 +1,30 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    sorted_array = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
+        else:
+            sorted_array.append(right[j])
+            j += 1
+
+    sorted_array.extend(left[i:])
+    sorted_array.extend(right[j:])
+
+    return sorted_array
+
+array = [38, -27, 4123, 3,29, 12382, 110]
+print(array)
+print(merge_sort(array))
